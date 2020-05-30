2f031bdbd866f0b233de272feb666569cf752def
tom57510 <thomas.progler15@gmail.com>
2020-05-30 12:24:56 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final ApiService apiService; // TODO: A utiliser
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+         return apiService.getUsers();
+        // TODO: A modifier
+
+    }
+
+    public void generateRandomUser() {
+        apiService.generateRandomUser();
+        // TODO: A modifier
+    }
+
+    public void deleteUser(User user) {
+         apiService.deleteUser(user);
+        // TODO: A modifier
+    }
+}
